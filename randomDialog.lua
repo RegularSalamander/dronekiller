d356371f33382b59894a6295781373669ab5f14e
Cameron Kellym <nopokelly@gmail.com>
2022-01-08 20:25:32 -0600

    more dialog + death dialog

--- randomDialog.lua
+++ randomDialog.lua
@@ -4,6 +4,14 @@ startDialog = {
         "hero", "Do these drones have any weaknesses?",
         "transmute", "Explosions.",
         "hero", "Perfect."
+    },
+    {
+        "transmute", "Our time is running out.",
+        "ceo", "You're already too late, Hero. Soon the city will be\nmine."
+    },
+    {
+        "hero", "What's the mission, Transmute?",
+        "transmute", "C-CORP's drones have already been released. But I\nhave an idea of how to stop them."
     }
 }
 
@@ -11,12 +19,16 @@ farDialog = {
     {
         "transmute", "You're about halfway to C-CORP's HQ.",
         "ceo", "I'm afraid I can't let you get any further.",
-        "transmute", "Be careful, Hero"
+        "transmute", "Be careful, Hero."
     },
     {
         "ceo", "I've been dying to test out these new toys.",
         "transmute", "What's he talking about?",
-        "hero", "I don't know but I don't wanna find out."
+        "hero", "I feel like I'm about to find out."
+    },
+    {
+        "ceo", "I'm always two steps ahead of you, Hero.",
+        "hero", "Guess I'll just have to speed up then."
     }
 }
 
@@ -34,11 +46,30 @@ highDialog = {
 goodComboDialog = {
     {
         "transmute", "Keep it up, Hero."
+    },
+    {
+        "hero", "Those drones never stood a chance."
     }
 }
 
 greatComboDialog = {
     {
         "ceo", "Nobody's giving out points for style."
+    },
+    {
+        "ceo", "Pace yourself, hero. No need to rush."
+    }
+}
+
+deadDialog = {
+    {
+        "ceo", "It was over before it began."
+    },
+    {
+        "transmute", "The resistance will live on.",
+        "ceo", "Good luck with that."
+    },
+    {
+        "ceo", "Game set, Hero."
     }
 }
